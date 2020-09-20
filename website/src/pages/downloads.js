b15cdb6a6fe5a8aa0236d350158a2a55106e24b0
Randy Oest <oest@amazingrando.com>
2020-09-20 18:57:04 -0400

    Update BE

--- website/src/pages/downloads.js
+++ website/src/pages/downloads.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Layout from '../components/layout';
+// import React from 'react';
+// import { useStaticQuery, graphql } from 'gatsby';
+// import Layout from '../components/layout';
 
-const Downloads = () => {
-  const data = useStaticQuery(graphql`
-    {
-      allNodeDownload(sort: { fields: title }) {
-        edges {
-          node {
-            title
-            field_link {
-              uri
-              url
-            }
-            field_hosted_elsewhere
-            field_file {
-              description
-              display
-            }
-            relationships {
-              field_file {
-                filename
-                id
-                size
-                uri {
-                  url
-                  value
-                }
-              }
-              field_dl_tags {
-                name
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
+// const Downloads = () => {
+//   const data = useStaticQuery(graphql`
+//     {
+//       allNodeDownload(sort: { fields: title }) {
+//         edges {
+//           node {
+//             title
+//             field_link {
+//               uri
+//               url
+//             }
+//             field_hosted_elsewhere
+//             field_file {
+//               description
+//               display
+//             }
+//             relationships {
+//               field_file {
+//                 filename
+//                 id
+//                 size
+//                 uri {
+//                   url
+//                   value
+//                 }
+//               }
+//               field_dl_tags {
+//                 name
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  console.log(data.allNodeDownload.edges);
+//   console.log(data.allNodeDownload.edges);
 
-  return (
-    <Layout>
-      <main className="main-content-wrapper">
-        <h1 className="page-title">Downloads</h1>
-      </main>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <main className="main-content-wrapper">
+//         <h1 className="page-title">Downloads</h1>
+//       </main>
+//     </Layout>
+//   );
+// };
 
-export default Downloads;
+// export default Downloads;
