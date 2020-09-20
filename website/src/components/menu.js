71836e65d25003bed70a10dd1e4dc94a09554bc1
Randy Oest <oest@amazingrando.com>
2020-09-20 18:57:13 -0400

    Prettier

--- website/src/components/menu.js
+++ website/src/components/menu.js
@@ -1,53 +1,51 @@
-import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
-import PropTypes from "prop-types"
+import React from 'react';
+import { StaticQuery, graphql, Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPlus, faMinus } from "@fortawesome/pro-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/pro-solid-svg-icons';
 
 function createMenuHierarchy(menuData, menuName) {
-  let tree = [],
-    mappedArr = {},
-    arrElem,
-    mappedElem
+  const tree = [];
+  const mappedArr = {};
+  let arrElem;
+  let mappedElem;
 
   // First map the nodes of the array to an object -> create a hash table.
   for (let i = 0, len = menuData.length; i < len; i++) {
-    arrElem = menuData[i].node
+    arrElem = menuData[i].node;
     if (arrElem.menu_name === menuName && arrElem.enabled === true) {
-      mappedArr[arrElem.drupal_id] = arrElem
+      mappedArr[arrElem.drupal_id] = arrElem;
       if (
         arrElem.drupal_parent_menu_item != null &&
         arrElem.drupal_parent_menu_item.includes(arrElem.bundle)
       ) {
-        let stripped_drupal_id = arrElem.drupal_parent_menu_item.replace(
-          arrElem.bundle + ":",
-          ""
-        )
+        const stripped_drupal_id = arrElem.drupal_parent_menu_item.replace(
+          `${arrElem.bundle}:`,
+          ''
+        );
         mappedArr[
           arrElem.drupal_id
-        ].drupal_parent_menu_item = stripped_drupal_id
+        ].drupal_parent_menu_item = stripped_drupal_id;
       }
-      mappedArr[arrElem.drupal_id]["children"] = []
+      mappedArr[arrElem.drupal_id].children = [];
     }
   }
 
-  for (let id in mappedArr) {
+  for (const id in mappedArr) {
     if (mappedArr.hasOwnProperty(id)) {
-      mappedElem = mappedArr[id]
+      mappedElem = mappedArr[id];
       // If the element is not at the root level, add it to its parent array of children.
       if (mappedElem.drupal_parent_menu_item) {
-        mappedArr[mappedElem.drupal_parent_menu_item]["children"].push(
-          mappedElem
-        )
+        mappedArr[mappedElem.drupal_parent_menu_item].children.push(mappedElem);
       }
       // If the element is at the root level, add it to first level elements array.
       else {
-        tree.push(mappedElem)
+        tree.push(mappedElem);
       }
     }
   }
-  return tree
+  return tree;
 }
 
 function buildLink(link, classBase) {
@@ -56,48 +54,48 @@ function buildLink(link, classBase) {
       <Link
         activeClassName="active"
         to={link.link.url}
-        className={classBase ? `${classBase}__link` : ""}
+        className={classBase ? `${classBase}__link` : ''}
       >
         {link.title}
       </Link>
-    )
-  } else if (!link.external && link.link.uri.includes("internal:")) {
+    );
+  }
+  if (!link.external && link.link.uri.includes('internal:')) {
     return (
       <Link
         activeClassName="active"
-        className={classBase ? `${classBase}__link` : ""}
-        to={link.link.uri.replace("internal:", "")}
+        className={classBase ? `${classBase}__link` : ''}
+        to={link.link.uri.replace('internal:', '')}
       >
         {link.title}
       </Link>
-    )
-  } else {
-    return (
-      <a
-        href={link.link.url}
-        className={classBase ? `${classBase}__link external` : "external"}
-      >
-        {link.title}
-      </a>
-    )
+    );
   }
+  return (
+    <a
+      href={link.link.url}
+      className={classBase ? `${classBase}__link external` : 'external'}
+    >
+      {link.title}
+    </a>
+  );
 }
 
 function buildMenu(menuArray, classBase) {
   if (!menuArray) {
-    return
+    return;
   }
-  let menu = []
-  for (let item in menuArray) {
+  const menu = [];
+  for (const item in menuArray) {
     if (menuArray[item].children.length !== 0) {
       menu.push(
         <li
           key={menuArray[item].drupal_id}
-          className={classBase ? `${classBase}__li` : ""}
+          className={classBase ? `${classBase}__li` : ''}
         >
           {buildLink(menuArray[item], classBase)}
           <button
-            className={classBase ? `${classBase}__show-menu` : "show-menu"}
+            className={classBase ? `${classBase}__show-menu` : 'show-menu'}
             aria-expanded="false"
           >
             <span className="closed">
@@ -109,38 +107,38 @@ function buildMenu(menuArray, classBase) {
           </button>
           <ul
             className={
-              classBase ? `${classBase}__ul ${classBase}__ul--child` : ""
+              classBase ? `${classBase}__ul ${classBase}__ul--child` : ''
             }
           >
             {buildMenu(menuArray[item].children, classBase)}
           </ul>
         </li>
-      )
+      );
     } else {
       menu.push(
         <li
           key={menuArray[item].drupal_id}
-          className={classBase ? `${classBase}__li` : ""}
+          className={classBase ? `${classBase}__li` : ''}
         >
           {buildLink(menuArray[item], classBase)}
         </li>
-      )
+      );
     }
   }
 
-  return menu
+  return menu;
 }
 
 function generateMenu(menuLinks, menuName, classBase) {
-  let menu
+  let menu;
 
   menu = createMenuHierarchy(
     menuLinks.allMenuLinkContentMenuLinkContent.edges,
     menuName
-  )
-  menu = buildMenu(menu, classBase)
+  );
+  menu = buildMenu(menu, classBase);
 
-  return menu
+  return menu;
 }
 
 const Menu = ({ menuName, classBase }) => (
@@ -160,7 +158,7 @@ const Menu = ({ menuName, classBase }) => (
               weight
               link {
                 uri
-                url
+                uri_alias
               }
               drupal_parent_menu_item
               bundle
@@ -171,23 +169,23 @@ const Menu = ({ menuName, classBase }) => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <nav className={classBase ? `${classBase}__nav` : menuName}>
-        <ul className={classBase ? `${classBase}__ul` : ""}>
+        <ul className={classBase ? `${classBase}__ul` : ''}>
           {generateMenu(data, menuName, classBase)}
         </ul>
       </nav>
     )}
   />
-)
+);
 
 Menu.propTypes = {
   menuName: PropTypes.string,
   classBase: PropTypes.string,
-}
+};
 
 Menu.defaultProps = {
   menuName: `main`,
-}
+};
 
-export default Menu
+export default Menu;
