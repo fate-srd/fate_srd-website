3f2f633d38be95d3bc1def950f76b581c58ef64f
Randy Oest <oest@amazingrando.com>
2020-11-08 22:41:37 -0500

    Anchor links

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -111,6 +111,13 @@ module.exports = {
         defer: false,
       },
     },
+    {
+      resolve: 'gatsby-plugin-anchor-links',
+      options: {
+        offset: -100,
+      },
+    },
+    `gatsby-plugin-remove-trailing-slashes`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- website/src/templates/article.js
+++ website/src/templates/article.js
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { graphql } from 'gatsby';
+import linkIcon from 'fate_srd-frontend/images/icons/link-solid.svg';
 import Layout from '../components/layout';
 import Aside from '../components/aside';
 import SEO from '../components/seo';
 
 function Article({ data }) {
   const pageData = data.allArticles.nodes[0];
-  const pageContent =
+  let pageContent =
     pageData.body.processed !== ''
       ? pageData.body.processed
       : pageData.body.value;
+  function replacer(match, p1, p2, p3, offset, string) {
+    const hash = p2
+      .replace(/ /g, '-')
+      .replace(/[?,:()“”"'’*]/g, '')
+      .replace(/^-/, '')
+      .toLowerCase();
+    return `<h${p1} id="${hash}">${p2}<a href="#${hash}" class="anchor" style="background-image: url(${linkIcon})"></a></h${p1}>`;
+  }
+  pageContent = pageContent.replace(/<h(\d+)>([^<>]*)<\/h(\d+)>/gi, replacer);
+
   const ruleBook = pageData.relationships.tags[0].name;
   const authorlist =
     data.allArticles.nodes[0].relationships.tags[0].relationships.node__author;
@@ -29,7 +40,6 @@ function Article({ data }) {
     </Layout>
   );
 }
-
 export const query = graphql`
   query($id: String!) {
     allArticles(filter: { id: { eq: $id } }) {
